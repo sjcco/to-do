594b32b915953b529e648c84554e0652703558f8
sjcco <73748496+sjcco@users.noreply.github.com>
2021-06-26 01:38:06 -0500

    Fix linter error ;

--- src/index.js
+++ src/index.js
@@ -2,8 +2,3 @@ import '@fortawesome/fontawesome-free/js/all';
 import './main.scss';
 
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
-
-
-var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
-  keyboard: false
-})
